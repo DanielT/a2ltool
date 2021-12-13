3f75b101b5938c22a7ac5e69fa2e7aa76671f802
Daniel Thaler <daniel@dthaler.de>
2021-12-13 22:00:03 +0100

    Fix symbol resolution for arrays using normal notation
    A2l files with versions earlier than 1.7 can't use "normal" notation for arrays
    (e.g. some_array[0]) because the angle brackets are not permitted in names.
    These files used the notation some_array._0_ instead.
    This is supported and works well.
    
    With files of version 1.7, it becomes possible to use '[' and ']', but symbols
    using these were not found correctly. The code incorrectly expected names like
    "some_array.[0]"

--- src/update/mod.rs
+++ src/update/mod.rs
@@ -3,6 +3,9 @@ use super::ifdata;
 use a2lfile::*;
 use std::collections::HashSet;
 
+#[cfg(test)]
+use std::collections::HashMap;
+
 mod axis_pts;
 mod blob;
 mod characteristic;
@@ -203,7 +206,14 @@ fn get_symbol_name_from_ifdata(ifdata_vec: &Vec<IfData>) -> Option<String> {
 // find a symbol in the elf_info data structure that was derived from the DWARF debug info in the elf file
 fn find_symbol<'a>(varname: &str, debug_data: &'a DebugData) -> Option<(u64, &'a TypeInfo)> {
     // split the a2l symbol name: e.g. "motortune.param._0_" -> ["motortune", "param", "_0_"]
-    let components: Vec<&str> = varname.split('.').collect();
+    let components = split_symbol_components(varname);
+
+    // find the symbol in the symbol table
+    find_symbol_from_components(&components, debug_data)
+}
+
+
+fn find_symbol_from_components<'a>(components: &Vec<&str>, debug_data: &'a DebugData) -> Option<(u64, &'a TypeInfo)> {
     // the first component of the symbol name is the name of the global variable.
     if let Some(varinfo) = debug_data.variables.get(components[0]) {
         // we also need the type in order to resolve struct members, etc.
@@ -225,10 +235,30 @@ fn find_symbol<'a>(varname: &str, debug_data: &'a DebugData) -> Option<(u64, &'a
 }
 
 
+// split the symbol into components
+// e.g. "my_struct.array_field[5][6]" -> [ "my_struct", "array_field", "[5]", "[6]" ]
+fn split_symbol_components(varname: &str) -> Vec<&str> {
+    let mut components: Vec<&str> = Vec::new();
+
+    for component in varname.split('.') {
+        if let Some(idx) = component.find('[') {
+            // "array_field[5][6]" -> "array_field", "[5][6]"
+            let (name, indexstring) = component.split_at(idx);
+            components.push(name);
+            components.extend(indexstring.split_inclusive(']'));
+        } else {
+            components.push(component);
+        }
+    }
+
+    components
+}
+
+
 // find the address and type of the current component of a symbol name
 fn find_membertype<'a>(
     typeinfo: &'a TypeInfo,
-    components: Vec<&str>,
+    components: &Vec<&str>,
     component_index: usize,
     address: u64
 ) -> Option<(u64, &'a TypeInfo)> {
@@ -253,7 +283,8 @@ fn find_membertype<'a>(
             TypeInfo::Array { dim, stride, arraytype, .. } => {
                 let mut multi_index = 0;
                 for idx_pos in 0 .. dim.len() {
-                    let indexval = get_index(components[component_index + idx_pos])?;
+                    let arraycomponent = components.get(component_index + idx_pos)?;
+                    let indexval = get_index(arraycomponent)?;
                     multi_index = multi_index * dim[idx_pos] as usize + indexval;
                 }
 
@@ -271,7 +302,7 @@ fn find_membertype<'a>(
 }
 
 
-// for some reason array indices in symbol names in a2l files are not written as [x], but as _x_
+// before ASAP2 1.7 array indices in symbol names could not written as [x], but only as _x_
 // this function will get the numerical index for either representation
 fn get_index(idxstr: &str) -> Option<usize> {
     if (idxstr.starts_with('_') && idxstr.ends_with('_')) ||
@@ -338,3 +369,90 @@ impl UpdateSumary {
     }
 }
 
+#[test]
+fn test_split_symbol_components() {
+    let result = split_symbol_components("my_struct.array_field[5][1]");
+    assert_eq!(result.len(), 4);
+    assert_eq!(result[0], "my_struct");
+    assert_eq!(result[1], "array_field");
+    assert_eq!(result[2], "[5]");
+    assert_eq!(result[3], "[1]");
+
+    let result2 = split_symbol_components("my_struct.array_field._5_._1_");
+    assert_eq!(result2.len(), 4);
+    assert_eq!(result2[0], "my_struct");
+    assert_eq!(result2[1], "array_field");
+    assert_eq!(result2[2], "_5_");
+    assert_eq!(result2[3], "_1_");
+}
+
+#[test]
+fn test_find_symbol_of_array() {
+    let mut dbgdata = DebugData {
+        types: HashMap::new(),
+        variables: HashMap::new()
+    };
+    // global variable: uint32_t my_array[2]
+    dbgdata.variables.insert(
+        "my_array".to_string(),
+        crate::dwarf::VarInfo { address: 0x1234, typeref: 1 });
+    dbgdata.types.insert(
+        1,
+        TypeInfo::Array {
+            arraytype: Box::new(TypeInfo::Uint32),
+            dim: vec![2],
+            size: 8, // total size of the array
+            stride: 4
+        }
+    );
+
+    // try the different array indexing notations
+    let result1 = find_symbol("my_array._0_", &dbgdata);
+    assert!(result1.is_some());
+    // C-style notation is only allowed starting with ASAP2 version 1.7, before that the '[' and ']' are not allowed in names
+    let result3 = find_symbol("my_array[0]", &dbgdata);
+    assert!(result3.is_some());
+}
+
+
+#[test]
+fn test_find_symbol_of_array_in_struct() {
+    let mut dbgdata = DebugData {
+        types: HashMap::new(),
+        variables: HashMap::new()
+    };
+    // global variable defined in C like this:
+    // struct {
+    //        uint32_t array_item[2];
+    // } my_struct;
+    let mut structmembers: HashMap<String, (TypeInfo, u64)> = HashMap::new();
+    structmembers.insert(
+        "array_item".to_string(),
+        (
+            TypeInfo::Array {
+                arraytype: Box::new(TypeInfo::Uint32),
+                dim: vec![2],
+                size: 8,
+                stride: 4
+            },
+            0
+        )
+    );
+    dbgdata.variables.insert(
+        "my_struct".to_string(),
+        crate::dwarf::VarInfo { address: 0xcafe00, typeref: 2 });
+    dbgdata.types.insert(
+        2,
+        TypeInfo::Struct {
+            members: structmembers,
+            size: 4
+        }
+    );
+
+    // try the different array indexing notations
+    let result1 = find_symbol("my_struct.array_item._0_", &dbgdata);
+    assert!(result1.is_some());
+    // C-style notation is only allowed starting with ASAP2 version 1.7, before that the '[' and ']' are not allowed in names
+    let result3 = find_symbol("my_struct.array_item[0]", &dbgdata);
+    assert!(result3.is_some());
+}
